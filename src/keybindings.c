08fa78f681d273d98a3563f07a82d527be628de4
Sam Light <samlight1994@gmail.com>
2022-05-19 20:53:54 +0100

    Read config, send to server process and exec commands when triggered

--- src/keybindings.c
+++ src/keybindings.c
@@ -11,6 +11,7 @@
 
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <sys/wait.h>
 
 #include <libevdev/libevdev.h>
 
@@ -18,22 +19,25 @@ struct socket {
     int fd;
 };
 
+struct state {
+    struct socket * sock;
+    struct keybind * keybinds;
+    char ** cmds;
+    size_t binds_count;
+};
+
 static struct socket * socket_new();
 static void socket_free(struct socket *);
 
-static void run_keybind(int);
-static int send_key_binds(int);
-static int handle_message(int);
-static void start_polling(struct socket *);
+static void state_load_config(struct state *, struct config *);
+static void state_free_keybinds(struct state *);
+static void state_cleanup(struct state *);
 
-
-struct keybind binds[] = {
-    {.index = 0, .keys = { KEY_LEFTCTRL, KEY_LEFTALT, KEY_LEFTSHIFT, KEY_L, 0, 0}},
-    {.index = 1, .keys = { KEY_LEFTCTRL, KEY_LEFTALT, KEY_LEFTSHIFT, KEY_D, 0, 0}},
-    {.index = 2, .keys = { KEY_VOLUMEDOWN, 0, 0, 0, 0, 0}},
-    {.index = 3, .keys = { KEY_VOLUMEUP, 0, 0, 0, 0, 0}},
-    {.index = 4, .keys = { KEY_MUTE, 0, 0, 0, 0, 0}},
-};
+static void run_cmd(char *);
+static void run_keybind(struct state *);
+static int send_key_binds(struct state *);
+static int handle_message(struct state *);
+static void start_polling(struct state *);
 
 struct socket *
 socket_new()
@@ -67,15 +71,54 @@ socket_free(struct socket * sock)
     free(sock);
 }
 
-int
-send_key_binds(int fd)
+void
+state_load_config(struct state * state, struct config * conf)
 {
-    int rc;
+    struct keybind * bind;
+    struct config_item * conf_item;
+    state->binds_count = conf->items_count;
+    state->keybinds = malloc(sizeof(struct keybind) * state->binds_count);
+    state->cmds = malloc(sizeof(char **) * state->binds_count);
+    for(size_t i = 0; i < state->binds_count; ++i) {
+        bind = &state->keybinds[i];
+        conf_item = &conf->items[i];
+        bind->index = i;
+        memcpy(bind->keys, conf_item->keys, sizeof(bind->keys));
+        state->cmds[i] = conf_item->cmd;
+        conf_item->cmd = NULL;
+    }
+}
+
+void
+state_free_keybinds(struct state * state)
+{
+    if(state->keybinds) {
+        free(state->keybinds);
+        state->keybinds = NULL;
+    }
+}
+
+void
+state_cleanup(struct state * state)
+{
+    for(size_t i = 0; i < state->binds_count; ++i) {
+        free(state->cmds[i]);
+    }
+    free(state->cmds);
+
+    state_free_keybinds(state);
+}
+
+int
+send_key_binds(struct state * state)
+{
+    int rc, fd;
     ipc_size amount;
     size_t buf_size = 254;
     char buf[buf_size];
 
-    amount = sizeof(binds) / sizeof(struct keybind);
+    fd = state->sock->fd;
+    amount = state->binds_count;
 
     rc = ipc_write_msg(fd, IPC_MSG_BIND);
     if(rc < 0) return rc;
@@ -83,13 +126,14 @@ send_key_binds(int fd)
     rc = write(fd, &amount, sizeof(amount));
     if(rc < 0) return rc;
 
-    rc = write(fd, &binds, sizeof(binds));
+    rc = write(fd, state->keybinds, sizeof(struct keybind) * amount);
     if(rc < 0) return rc;
 
     rc = ipc_read_timeout(fd, &buf, buf_size);
     if(rc < 0) return rc;
 
     if(ipc_test_msg(buf, IPC_MSG_RECV)) {
+        state_free_keybinds(state);
         return 0;
     }
 
@@ -97,44 +141,64 @@ send_key_binds(int fd)
 }
 
 void
-run_keybind(int fd)
+run_cmd(char * cmd)
+{
+    pid_t pid = -1;
+
+    pid = fork();
+    if(pid == 0) {
+        setsid();
+        if(fork() == 0) {
+            execlp("sh", "sh", "-c", cmd, NULL);
+        }
+        else _exit(0);
+    }
+    else if(pid > 0) wait(NULL);
+}
+
+void
+run_keybind(struct state * state)
 {
     keybind_index index;
-    int rc;
+    int rc, fd;
+
+    fd = state->sock->fd;
 
     rc = ipc_read_timeout(fd, &index, sizeof(keybind_index));
     if(rc < 0) return;
 
-    log_info("trigger: %i", index);
+    if(index >= 0 && index < state->binds_count) {
+        run_cmd(state->cmds[index]);
+    }
 }
 
 int
-handle_message(int fd)
+handle_message(struct state * state)
 {
     const size_t bufsize = 256;
     char buf[bufsize];
 
-    read(fd, &buf, bufsize);
+    read(state->sock->fd, &buf, bufsize);
     if(ipc_test_msg(buf, IPC_MSG_READY)) {
         log_info("READY");
-        return send_key_binds(fd);
+        return send_key_binds(state);
     }
 
     if(ipc_test_msg(buf, IPC_MSG_TRIG)) {
-        run_keybind(fd);
+        run_keybind(state);
     }
 
     return 0;
 }
 
 void
-start_polling(struct socket * sock)
+start_polling(struct state * state)
 {
     int rc;
     struct pollfd pfds[1];
     struct pollfd * pfd = &pfds[0];
 
-    pfd->fd = sock->fd;
+    pfd->fd = state->sock->fd;
     pfd->events = POLLIN;
 
     while(signal_running) {
@@ -153,16 +217,15 @@ start_polling(struct socket * sock)
             break;
         }
 
-        rc = handle_message(pfd->fd);
+        rc = handle_message(state);
         if(rc < 0) break;
     }
 }
 
-
 int main(int argi, char** argv) {
     int rc;
-    struct socket * sock;
-    struct config_item * config_items;
+    struct config * config;
+    struct state state;
 
     rc = signal_setup_actions();
     if(rc < 0) {
@@ -170,21 +233,25 @@ int main(int argi, char** argv) {
         exit(-1);
     }
 
-    config_items = read_config();
-    if(config_items == NULL) {
+    config = config_read();
+    if(config == NULL) {
         perror("Failed to read config");
         exit(-1);
     }
 
-    sock = socket_new();
-    if(sock == NULL) {
+    state_load_config(&state, config);
+    config_free(config);
+
+    state.sock = socket_new();
+    if(state.sock == NULL) {
         perror("Failed to open IPC socket");
         exit(-1);
     }
 
-    start_polling(sock);
+    start_polling(&state);
 
-    socket_free(sock);
+    socket_free(state.sock);
+    state_cleanup(&state);
 
     return 0;
 }
